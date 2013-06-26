4de6548ddbddd172cb5fbaf69f81af2366f5cb33
Marcelo G Silva <mgswolf@gmail.com>
2013-06-25 22:00:15 -0300

    index admin users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def t_field(model,field)
+    t("activerecord.attributes.#{model}.#{field}")
+  end
 end
